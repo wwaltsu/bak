0b3cad676c7496bc3eec380eaad626133c36ab0b
Waltsu <waltsumarcus@gmail.com>
2022-10-10 22:14:43 +0300

    routet toimii

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-const http = require('http')
 const ruuat = 'https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentWeek?costNumber=3060&language=fi' 
+const express = require('express')
+const app = express()
+const cors = require('cors');
+
 
 const { parse } = require('rss-to-json');
-// async await
-(async () => {
 
-    var rss = await parse('https://www.foodandco.fi/modules/MenuRss/MenuRss/CurrentWeek?costNumber=3060&language=fi');
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Oppari proju')
+})
+
+
+// ruokalista pitäisi saada rss
+var rss = parse(ruuat);
+
+app.get('/menu', (request, res) => {
+  res.send(rss)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+
+
+// async await
+/* (async () => {
+
+    var rss = await parse(ruuat);
 
     console.log(JSON.stringify(rss, null, 3));
 
     const app = http.createServer((request, response) => {
       response.writeHead(200, { 'Content-Type': 'application/json' })
       response.end(JSON.stringify(rss))
-    }) 
+    })
+    
+    app.get('/', (req, res) => {
+      res.send('<h1>Hello World!</h1>')
+    })
     
     const PORT = 3001
     app.listen(PORT)
     console.log(`Server running on port ${PORT}`)
     
-})();
+})(); */
 
 
